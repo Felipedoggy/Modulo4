e1dfd367ec67cf811b163ab7752ea3e70935e640
Felipe <fcurcci@gmail.com>
2026-03-23 14:53:50 -0300

    commit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '64.181.162.120',
+  host: process.env.DB_HOST || '163.176.55.67',
   user: process.env.DB_USER || 'remoto',
   password: process.env.DB_PASSWORD || '12345senha',
   database: process.env.DB_NAME || 'despesasdb',
