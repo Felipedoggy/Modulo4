6a52db3810ba4318e8e62ddc7fc8f78d1d61fd9d
Felipe <fcurcci@gmail.com>
2026-03-23 11:45:00 -0300

    initial

---
+++ Backend/config/db.js
@@ -0,0 +1,15 @@
+// db.js
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || '64.181.162.120',
+  user: process.env.DB_USER || 'remoto',
+  password: process.env.DB_PASSWORD || '12345senha',
+  database: process.env.DB_NAME || 'despesasdb',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool;
